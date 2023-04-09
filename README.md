fba72508c48f2d93d4344846939edca2b7a09f65
achrysaetos <achrysaetos@yahoo.com>
2023-04-09 14:13:40 -0400

    add poc

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # README
+Simple drawing app.
+1. Run the playback in the recorder repo (create local server)
+2. Open index.html in this repo
+3. Open http://127.0.0.1:4444/ in another tab
+
+![](demo.mov)
+
+## Sources
+Docs here: https://cpsc484-584-hci.gitlab.io/s23/display_tutorial/
+Enable playback: https://github.com/Yale-CPSC484-HCI/recorder
+* Record: `pipenv run python src/main.py --websocket-server cpsc484-03.yale.internal:8888 --data-path data/sample3 --mode record`
+* Playback: `pipenv run python src/main.py --data-path data/sample3 --mode play`
--- sketch.js
+++ sketch.js
@@ -1,18 +1,19 @@
 
-var socket = new WebSocket("ws://127.0.0.1:4444/frames")
+// "ws://cpsc484-01.yale.internal:8888/frames" (live) or "ws://127.0.0.1:4444/frames" (recorded)
+var socket = new WebSocket("ws://127.0.0.1:4444/frames") 
 
-last_pos = [500,500]
+last_pos = [500,500] // start centered
 
 socket.onmessage = (event) => {
-  let data = JSON.parse(event.data)
-  if (data.people.length > 0){
-    console.log("Person!")
+  let data = JSON.parse(event.data) // get web socket stream
+  if (data.people.length > 0){ // found a person
 
-    x = -data.people[0].joints[15].position.x+500
-    y = data.people[0].joints[15].position.y
-    console.log(x)
-    console.log(y)
+    // get right hand position (and normalize it!)
+    x = -data.people[0].joints[15].position.x+600
+    y = data.people[0].joints[15].position.y+300
+    console.log("("+Math.floor(x)+","+Math.floor(y)+")")
 
+    // draw line
     stroke(255);
     line(last_pos[0], last_pos[1], x, y);
 
@@ -20,17 +21,8 @@ socket.onmessage = (event) => {
   }
 }
 
+// initial blank canvas
 function setup() {
-  createCanvas(1920, 1080);
+  createCanvas(1200, 900);
   background(102);
 }
-
-function draw() {
-  stroke(255);
-  if (mouseIsPressed === true) {
-    line(mouseX, mouseY, pmouseX, pmouseY);
-    console.log(mouseY)
-    console.log(pmouseY)
-  }
-}
-

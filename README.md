fcc8ced432b393b7815349c1a21519a8d03122a6
achrysaetos <achrysaetos@yahoo.com>
2023-04-09 16:22:39 -0400

    thicken lines

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ Simple drawing app. Demo vid here: https://youtu.be/_1G5D871B5Q
 2. Open index.html in this repo
 3. Open http://127.0.0.1:4444/ in another tab
 
+# Necessary functionality
+* Draw - left hand closed, right hand open
+* Move - left hand open, right hand open
+* Erase - left hand closed, right hand closed
+* Color
+
 ## Sources
 Docs here: https://cpsc484-584-hci.gitlab.io/s23/display_tutorial/ \
 Enable playback: https://github.com/Yale-CPSC484-HCI/recorder
--- sketch.js
+++ sketch.js
@@ -2,22 +2,23 @@
 // "ws://cpsc484-01.yale.internal:8888/frames" (live) or "ws://127.0.0.1:4444/frames" (recorded)
 var socket = new WebSocket("ws://127.0.0.1:4444/frames") 
 
-last_pos = [500,500] // start centered
+last_x = 500; last_y = 500 // track last position
 
 socket.onmessage = (event) => {
   let data = JSON.parse(event.data) // get web socket stream
   if (data.people.length > 0){ // found a person
 
-    // get right hand position (and normalize it!)
+    // get right hand position (and normalize it)
     x = -data.people[0].joints[15].position.x+600
     y = data.people[0].joints[15].position.y+300
     console.log("("+Math.floor(x)+","+Math.floor(y)+")")
 
-    // draw line
-    stroke(255);
-    line(last_pos[0], last_pos[1], x, y);
+    // draw continous line
+    for(let i=1; i<100; i++){
+      draw(last_x+i/100*(x-last_x), last_y+i/100*(y-last_y))
+    }
 
-    last_pos = [x,y]
+    last_x = x; last_y = y
   }
 }
 
@@ -25,4 +26,11 @@ socket.onmessage = (event) => {
 function setup() {
   createCanvas(1200, 900);
   background(102);
+  noStroke();
+  fill(0, 102);
+}
+
+function draw(x, y) {
+  fill(255);
+  ellipse(x, y, 30, 30);
 }

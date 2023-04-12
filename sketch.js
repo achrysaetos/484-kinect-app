f5f459c44a6128768ff2491d86d7db60dad21d67
natasharavinand <natasharavinand@gmail.com>
2023-04-12 15:25:08 -0400

    add boxes

--- sketch.js
+++ sketch.js
@@ -8,45 +8,27 @@ socket.onmessage = (event) => {
   let data = JSON.parse(event.data) // get web socket stream
   if (data.people.length > 0){ // found a person
 
-    // get right hand position (and normalize it)
-    x = -data.people[0].joints[15].position.x+600
-    y = data.people[0].joints[15].position.y+300
-    console.log("("+Math.floor(x)+","+Math.floor(y)+")")
-
-    // keep track of open/closed hands
-    drawing = true // data.people[0].joints[16].position.y-data.people[0].joints[17].position.y < 0
-    erasing = false // data.people[0].joints[16].position.y-data.people[0].joints[17].position.y > 0
-
-    if (drawing){
-      for(let i=1; i<100; i++){
-        draw(last_x+i/100*(x-last_x), last_y+i/100*(y-last_y))
-      }
-    } else if (erasing){
-      for(let i=1; i<100; i++){
-        erase(last_x+i/100*(x-last_x), last_y+i/100*(y-last_y))
-      }
-    } else if (moving){
-      console.log("Moving...") // invisible, no need to implement
-    }
-
-    last_x = x; last_y = y // update last position
   }
 }
 
-// initial blank canvas
+
+
 function setup() {
-  createCanvas(1200, 900);
-  background(244, 244, 232);
-  noStroke();
-  fill(244, 244, 232);
+      
+  // Create Canvas of size 600*600
+  createCanvas(1500, 700, WEBGL);
 }
+ 
+function draw() {
+    
+  // Set background color
+  background(200);
+   
+  // Set fill color of box
+  fill('green');
+   
+  square(-500, -150, 300);
 
-function draw(x, y) {
-  fill(52, 165, 111);
-  ellipse(x, y, 30, 30);
-}
-
-function erase(x, y) {
-  fill(244, 244, 232);
-  ellipse(x, y, 60, 60);
+  fill('purple');
+  square(50, -150, 300);
 }

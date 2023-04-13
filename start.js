9b1b014a4b127743db1fee5893cd1ca19d401ec6
riceeye <ryantsai123@gmail.com>
2023-04-13 12:50:29 -0400

    homepage and start button

---
+++ start.js
@@ -0,0 +1,32 @@
+// "ws://cpsc484-01.yale.internal:8888/frames" (live) or "ws://127.0.0.1:4444/frames" (recorded)
+var socket = new WebSocket("ws://127.0.0.1:4444/frames")
+
+
+
+socket.onmessage = (event) => {
+  let data = JSON.parse(event.data) // get web socket stream
+  if (data.people.length > 0){ // found a person
+
+    // get right hand position (and normalize it)
+    x = -data.people[0].joints[15].position.x+600
+    y = data.people[0].joints[15].position.y+300
+    console.log("("+Math.floor(x)+","+Math.floor(y)+")")
+
+    if (Math.sqrt((x - 600)^2 + (y - 450)^2) <= 50){
+      window.location.href = "index.html"
+    }
+
+    update(x, y) // update tracker
+  }
+}
+
+/* p5.js functions */
+
+let circle;
+
+function update(x, y){
+    circle = document.getElementById('circle'); 
+
+    circle.style.left = x + 'px';
+    circle.style.top = y + 'px';
+}
